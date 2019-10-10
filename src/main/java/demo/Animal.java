3204dd30f261c28c69c8e8efc90f2c3610eaa88a
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2019-10-10 09:21:33 -0400

    first commit

---
+++ src/main/java/demo/Animal.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by makerofapps on 10/10/19.
+ */
+public class Animal {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
